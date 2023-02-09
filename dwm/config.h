507f5257560a6b2859ebd8f01940cbcb3c8d7197
lugar <Lug3Zz@github.com>
2023-02-09 22:46:08 +0800

    add  winIcon patch and add st Icon

--- dwm/config.h
+++ dwm/config.h
@@ -5,13 +5,15 @@ static const unsigned int borderpx  = 0;        /* border pixel of windows */
 static const unsigned int snap      = 10;       /* snap pixel */
 static const unsigned int gappih    = 8;       /* horiz inner gap between windows */
 static const unsigned int gappiv    = 8;       /* vert inner gap between windows */
-static const unsigned int gappoh    = 10;       /* horiz outer gap between windows and screen edge */
-static const unsigned int gappov    = 10;       /* vert outer gap between windows and screen edge */
+static const unsigned int gappoh    = 5;       /* horiz outer gap between windows and screen edge */
+static const unsigned int gappov    = 5;       /* vert outer gap between windows and screen edge */
 static       int smartgaps          = 1;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 0;        /* 0 means bottom bar */
 static const Bool viewontag         = True;     /* Switch view on tag switch */
 static const char *fonts[]          = { /*"monospace:size=12",*/"Symbols Nerd Font:type=2048-em:pixelsize=17:antialias=true:automint=true",
+#define ICONSIZE bh   /* icon size */
+#define ICONSPACING 5 /* space between icon and title */
                                                                  "WenQuanYi Zen Hei Mono:size=12:antialias=true:automint=true",
                                                                  "FiraCode Nerd Font Mono:size=12" };
 static const char dmenufont[]       = "FiraCode Nerd Font Mono:size=12";
