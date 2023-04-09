b20fc0d7cd7b3cbcba0106a0a4918ab82345f68c
Brown Lu <Lug3Zz@github.com>
2023-04-09 18:16:50 +0800

    new dmenu

--- dmenu/config.h
+++ dmenu/config.h
@@ -1,37 +1,35 @@
 /* See LICENSE file for copyright and license details. */
 /* Default settings; can be overriden by command line. */
 
-static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
+static int topbar = 1; /* -b  option; if 0, dmenu appears at bottom     */
 static const unsigned int alpha = 0xf0;
-static int colorprompt = 1;                /* -p  option; if 1, prompt uses SchemeSel, otherwise SchemeNorm */
-static int centered = 1;                    /* -c option; centers dmenu on screen */
-static int min_width = 500;                    /* minimum width when centered */
+/* Size of the window border */
+static const unsigned int border_width = 0;
+static int colorprompt =
+    0; /* -p  option; if 1, prompt uses SchemeSel, otherwise SchemeNorm */
+static int centered = 1;    /* -c option; centers dmenu on screen */
+static int min_width = 500; /* minimum width when centered */
 /* -fn option overrides fonts[0]; default X11 font or font set */
-static const char *fonts[] = {
-	"FiraCode Nerd Font Mono:size=10:style=Regular"
-};
-static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
+static const char *fonts[] = {"FiraCode Nerd Font Mono:size=10:style=Regular"};
+static const char *prompt =
+    NULL; /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
-		/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
-	[SchemeOut] = { "#000000", "#00ffff" },
+    /*     fg         bg       */
+    [SchemeNorm] = {"#f8f8f2", "#282a36"},
+    [SchemeSel] = {"#f8f8f2", "#6272a4"},
+    [SchemeOut] = {"#000000", "#00ffff"},
 };
 static const unsigned int alphas[SchemeLast][2] = {
-	[SchemeNorm] = { OPAQUE, alpha },
-	[SchemeSel] = { OPAQUE, alpha },
-	[SchemeOut] = { OPAQUE, alpha },
+    [SchemeNorm] = {OPAQUE, alpha},
+    [SchemeSel] = {OPAQUE, alpha},
+    [SchemeOut] = {OPAQUE, alpha},
 };
 
-
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
-static unsigned int lines      = 1 ;
+static unsigned int lines = 10;
 
 /*
  * Characters not considered part of a word while deleting words
  * for example: " /?\"&[]"
  */
 static const char worddelimiters[] = " ";
-
-/* Size of the window border */
-static const unsigned int border_width = 3;
